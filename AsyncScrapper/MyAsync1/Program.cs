9db8c9db792571ce6dc0bfbf6a425a100fe441e1
Eugene Murray <eugene.murray@gmail.com>
2013-09-19 11:08:36 +0100

    add code

---
+++ AsyncScrapper/MyAsync1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAsync1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AccessWebAsync().Result);
+
+            Console.ReadKey();
+        }
+
+        private static async Task<string> AccessWebAsync()
+        {
+            var httpClient = new HttpClient();
+
+            Console.WriteLine("call url");
+            Task<string> getStringTask = httpClient.GetStringAsync("http://www.bbc.co.uk/sport/0/football/gossip/");
+
+            Console.WriteLine("await");
+            var urlContents = await getStringTask;
+
+            Console.WriteLine("return");
+            return urlContents;
+        }
+    }
+}
